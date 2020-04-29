c66c702239be6d4a7df9c1ecbad390a1e5127153
vishalpwaman <vishalpwaman1997@gmail.com>
2020-04-29 18:31:59 -0400

    Mood test Happy

--- Moodanalysertest.java
+++ Moodanalysertest.java
@@ -5,4 +5,9 @@ public class Moodanalysertest {
     public String varificationSadTest(String Message) {
         return "SAD";
     }
+
+ public String varificationHappyTest(String your_mood_are_happy) {
+        return "HAPPY";
+    }
+
 }
